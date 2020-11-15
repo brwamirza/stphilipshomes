2e4ecf24f0be19ab66e45113fe9e5e0a9beccbfd
brwa kawa <mirzabrwa@gmail.com>
2020-11-15 22:11:05 +0300

    added navabar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Navbar from "./components/navbar.component";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
